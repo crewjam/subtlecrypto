7c8c71eb7bfd25ad667fde3f089297e5cd5a806e
Mike Houston <m@hou.st>
2016-08-29 16:49:38 +0000

    Updates to use browsertests package

--- keygen_test.go
+++ keygen_test.go
@@ -3,95 +3,67 @@ package subtlecrypto
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"net/url"
-	"os"
-	"path"
 	"testing"
 	"time"
 
-	"github.com/gopherjs/gopherjs/js"
+	_ "bitbucket.org/mikehouston/browsertests"
 )
 
 func TestAESKeyGen(t *testing.T) {
-	println("Generate AES Key")
+	t.Log("Generate AES Key")
 	key, err := GenerateSymmetricKey(AES_CBC(AES_256), true)
 	if err != nil {
 		t.Error(err)
 	}
 	str, _ := key.Export(PKCS8)
-	println(str)
+	t.Log(str)
 }
 
 func TestRSAKeyGen(t *testing.T) {
-	println("Generate RSA KeyPair")
+	t.Log("Generate RSA KeyPair")
 	start := time.Now()
 	keypair, err := GenerateRSAKeyPair(RSASSA_PKCS1_v1_5(2048, SHA_256), true)
 	if err != nil {
 		t.Error(err)
 	}
-	println("Took", time.Now().Sub(start).String())
+	t.Log("Took", time.Now().Sub(start).String())
 
 	public, _ := keypair.PublicKey.Export(JWK)
-	println("Public:", public)
+	t.Log("Public:", public)
 	private, _ := keypair.PrivateKey.Export(JWK)
-	println("Private:", private)
+	t.Log("Private:", private)
 }
 
 func TestECKeyGen(t *testing.T) {
-	println("Generate EC KeyPair")
+	t.Log("Generate EC KeyPair")
 	start := time.Now()
 	keypair, err := GenerateECKeyPair(ECDSA(P_256), true)
 	if err != nil {
 		t.Error(err)
 	}
-	println("Took", time.Now().Sub(start).String())
+	t.Log("Took", time.Now().Sub(start).String())
 
 	public, _ := keypair.PublicKey.Export(JWK)
-	println("Public:", public)
+	t.Log("Public:", public)
 	private, _ := keypair.PrivateKey.Export(JWK)
-	println("Private:", private)
+	t.Log("Private:", private)
 }
 
 func TestStdlibKeyGen(t *testing.T) {
 	t.Skip("Stdlib keygen is very slow - skipping")
 
-	println("Generate RSA KeyPair using crypto/rsa")
+	t.Log("Generate RSA KeyPair using crypto/rsa")
 	start := time.Now()
 	keypair2, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		t.Error(err)
 	}
-	println("Took", time.Now().Sub(start).String())
-	println(keypair2)
+	t.Log("Took", time.Now().Sub(start).String())
+	t.Log(keypair2)
 }
 
 func BenchmarkRSAKeyGen_Subtle(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		GenerateRSAKeyPair(RSASSA_PKCS1_v1_5(2048, SHA_256), true)
-	}
-}
-
-// See https://github.com/shurcooL/eX0/blob/17918f464a7c40e276bed59bb2765dc5186409e3/eX0-go/main_js.go#L26-L48
-func queryToArgs() []string {
-	u, err := url.Parse(js.Global.Get("location").Get("href").String())
-	if err != nil {
-		panic(err)
-	}
-	args := []string{path.Base(u.Path)} // First element is the process name.
-	for k, vs := range u.Query() {
-		for _, v := range vs {
-			args = append(args, k)
-			if v != "" {
-				args = append(args, v)
-			}
-		}
-	}
-	return args
-}
-
-func init() {
-	os.Args = queryToArgs()
-	if len(os.Args) == 1 {
-		os.Args = append(os.Args, "-test.v") // Default mode when no parameters are provided.
 	}
 }
